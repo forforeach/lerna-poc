ca47a4d87c7ed9d2b94a608800d680a6e2ef47a2
forforeach <forforeach@gmail.com>
2019-05-25 16:57:28 +0300

    feat(ui-flows): add cool footer

--- packages/ui-flows/src/App.vue
+++ packages/ui-flows/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <footer>Add my cool footer</footer>
   </div>
 </template>
 
